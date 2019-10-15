121a4cb8c46c0954162c4858cb6c1845c8f71ef3
Kalysh <503292@ukr.net>
2019-10-16 01:23:14 +0300

    ADD _ draw one pet

--- src/pages/Pet.js
+++ src/pages/Pet.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import pets from '../pets.json';
+import css from './Pages.module.css';
 
 class Pet extends Component {
-  state = {};
+  state = { onePet: [] };
+
   render() {
-    console.log(this.props.match);
-    return <>InfoPet</>;
+    const pet = pets.find(pet => pet.id === this.props.match.params.idPets);
+    console.log(pet);
+    return (
+      <>
+        <div className={css.petPageMain}>
+          {/* <ReturnBtn /> */}
+          <p className={css.namePet}>All about {pet.name}</p>
+          <div className={css.cardPet}>
+            <img className={css.imagePet} src={pet.image} alt="pet" />
+            <div className={css.about}>
+              <p className={css.aboutPet}>
+                Age: <span className={css.dataPet}>{pet.age}</span>
+              </p>
+              <p className={css.aboutPet}>
+                Gender: <span className={css.dataPet}>{pet.gender}</span>
+              </p>
+              <p className={css.aboutPet}>
+                Color: <span className={css.dataPet}>{pet.color}</span>
+              </p>
+              <p className={css.aboutPet}>
+                Breed: <span className={css.dataPet}>{pet.breed}</span>
+              </p>
+            </div>
+          </div>
+          <p className={css.descriptionPet}>{pet.description}</p>
+        </div>
+      </>
+    );
   }
 }
 
